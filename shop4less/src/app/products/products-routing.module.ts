911b7e9e0e8e11a180ebf7fc367b3b0ad7968806
devloper7306 <devops.usa7306@gmail.com.com>
2023-01-22 16:24:43 -0500

    new

--- shop4less/src/app/products/products-routing.module.ts
+++ shop4less/src/app/products/products-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
 import { ProductsComponent } from './products.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { ViewAllProductByCategoryComponent } from './view-all-product-by-category/view-all-product-by-category.component';
+import { ViewAllProductByDateComponent } from './view-all-product-by-date/view-all-product-by-date.component';
+import { ViewAllProductComponent } from './view-all-product/view-all-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'create-product', component: CreateProductComponent },
+  { path: 'delete-product', component: DeleteProductComponent },
+  { path: 'view-product', component: ViewProductComponent },
+  { path: 'update-product', component: UpdateProductComponent },
+  { path: 'view-all-product', component: ViewAllProductComponent },
+  {
+    path: 'view-all-product-by-category',
+    component: ViewAllProductByCategoryComponent,
+  },
+  {
+    path: 'view-all-product-by-date',
+    component: ViewAllProductByDateComponent,
+  },
+  { path: 'view-all-product', component: ViewAllProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProductsRoutingModule { }
+export class ProductsRoutingModule {}
